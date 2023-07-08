1e0a8b381d780018871c43ffcf1cbd25e2a02557
Mat Gargano <mat@statenweb.com>
2023-07-08 14:51:17 -0400

    update for 1.0.5

--- qpfr/actions.php
+++ qpfr/actions.php
@@ -9,9 +9,8 @@ class Actions {
 	public function init(){
 		$this->attach_hooks();
 	}
+
 	public function attach_hooks(){
-
-
 		// hook into the update_option action for the following actions so we can set a timestamp to use for images
 		$update_option_actions = [
 			'redis_object_cache_flush',
@@ -29,16 +28,14 @@ class Actions {
 		add_filter('wp_calculate_image_srcset', array($this, 'append_image_src_srcset'), 10, 1);
 		add_filter('wp_get_attachment_image_src', array($this, 'append_image_src'), 10, 4);
 		add_filter('wp_get_attachment_image_attributes', array($this, 'append_image_attributes'), 999 , 3);
-
 	}
 
 	// update the option with the current timestamp
-
 	public function update_option(){
 		update_option(self::OPTION_NAME, time());
 	}
 
-	//
+	// append the query string to the image urls in srcset
 	public function append_image_src_srcset($sources) {
 		$new_sources = [];
 		foreach ($sources as $key => $source) {
@@ -56,22 +53,13 @@ class Actions {
 
 	// append the query string to the image attributes
 	function append_image_attributes($attr, $attachment, $size) {
-
 		$attr['src'] = self::append_query_string($attr['src']);
-
 		return $attr;
 
 	}
 
 	// append the query string to the url
 	private static function append_query_string($url) {
-		// if the query parameter already exists, let's leave
-		if (strpos($url, self::OPTION_NAME) !== false) {
-			return $url;
-		}
-
 		return add_query_arg(self::OPTION_NAME, get_option(self::OPTION_NAME), $url);
 	}
-
-
 }
